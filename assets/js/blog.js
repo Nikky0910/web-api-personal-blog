a5abace136233af0bb5ed132d650c76ee9fe7346
Marianna <mariannaniko1092@gmail.com>
2024-09-02 21:23:43 -0400

    Add block.js to project

---
+++ assets/js/blog.js
@@ -0,0 +1,33 @@
+//take it from the local storage 
+//display it in the blog page
+// change the background to dark when clicking the toggle
+
+let blogsList= localStorage.getItem('blogsArray');
+let updatedBlogsList= JSON.parse(blogsList);
+
+for (let element of updatedBlogsList){
+    let span = document.createElement('span');
+    span.innerHTML = element.username;
+
+    let p = document.createElement('p');
+    p.innerText = "Posted by: ";
+    p.appendChild(span);
+
+    let footer = document.createElement('footer');
+    footer.appendChild(p);
+
+    let pContent = document.createElement('p');
+    pContent.innerHTML = element.content;
+
+    let h3 = document.createElement('h3');
+    h3.innerHTML = element.username;
+
+    let article = document.createElement('article');
+    article.appendChild(h3);
+    article.appendChild(pContent);
+    article.appendChild(footer);
+
+    let main= document.querySelector('main')
+    main.appendChild(article);
+
+}
