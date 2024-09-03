e595f6cee47aae66b5da004e52f77d1c9576ce36
Marianna <mariannaniko1092@gmail.com>
2024-09-02 21:24:15 -0400

    Add logic.js to project

---
+++ assets/js/logic.js
@@ -0,0 +1,38 @@
+let toggle = document.getElementById('theme');
+let mode = 'light';
+
+toggle.addEventListener('click', function(){
+    let body= document.body;
+    let main = document.querySelector('main')
+    let header = document.querySelector('header')
+    let footer = document.getElementById('footer')
+    let articles = document.querySelectorAll('article')
+    let button = document.querySelector('button')
+
+
+    if (mode==='light'){
+        mode= 'dark';
+        body.setAttribute('class', mode);
+        main.setAttribute('class', mode);
+        header.setAttribute('class', mode);
+        footer.setAttribute('class', mode);
+        button.setAttribute('class', mode);
+        for (let article of articles) {
+            article.setAttribute('class', mode);
+            let h3 = article.querySelector('h3');
+            h3.setAttribute('class', mode);
+        }
+    }else {
+        mode = 'light';
+        body.setAttribute('class', mode);
+        main.setAttribute('class', mode);
+        header.setAttribute('class', mode);
+        footer.setAttribute('class', mode);
+        button.setAttribute('class', mode);
+        for (let article of articles) {
+            article.setAttribute('class', mode);
+            let h3 = article.querySelector('h3');
+            h3.setAttribute('class', mode);
+        }
+    }
+})
